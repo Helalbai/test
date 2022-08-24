23e2e6109e080f3fa572c23eed0925ac60b6fcac
unknown <hayatr@gmail.com>
2022-08-24 00:09:28 -0400

    this is my git

---
+++ src/test1/Test2.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        int price = 200;
+        price = 250;
+        price = 300;
+
+        int price1 = 500;
+
+        System.out.println("Price Value = " + price);
+        System.out.println("Price1 Value = " + price1);
+
+    }
+}
